6524c0013770c6b4bb6c547111771d64446ee989
js <js201910271836@outlook.com>
2021-08-26 09:33:30 +0200

    26-08-21 9:33:30

--- push.bat
+++ push.bat
@@ -3,8 +3,9 @@ chcp 65001
 
 ::@param token Github
 
-set a=1;
-set "g=https://github.com/js202005082300/Aide-memoires.git"
+set "UTILISATEUR=js202005082300"
+set "DEPOT=Aide-memoires"
+set "LIEN=https://github.com/%UTILISATEUR%/%DEPOT%.git"
 set d=%date:~0,2%-%date:~3,2%-%date:~6,4%
 set t=%time:~0,2%:%time:~3,2%:%time:~6,2%
 set t=%t: =%
@@ -14,14 +15,15 @@ if not exist ".git" (
     git config --global user.email "js201910271836@outlook.com"
     git config --global user.name "js"
     git branch -M main
-    if not exist "README.md" ( echo # Aide-mémoires>> README.md )
+    if not exist "README.md" ( echo # %DEPOT%>> README.md )
 )
 
-if "%~1" NEQ "" ( echo git remote add origin https://js202005082300:%~1@github.com/js202005082300/Aide-memoires.git )
+if "%~1" NEQ "" ( echo git remote add origin https://%UTILISATEUR%:%~1@github.com/%UTILISATEUR%/%DEPOT%.git )
 
-git pull %g%
+git pull %LIEN%
 git add *
 git commit -a -m "%d% %t%"
-git push %g%
+git push %LIEN%
 
+@echo ON
 exit
