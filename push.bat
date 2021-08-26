d9e39bcda8987be5d7d78d6b166368bf7802868c
js <js201910271836@outlook.com>
2021-08-26 09:25:04 +0200

    26-08-21 9:25:02

--- push.bat
+++ push.bat
@@ -5,7 +5,6 @@ chcp 65001
 
 set a=1;
 set "g=https://github.com/js202005082300/Aide-memoires.git"
-set "test=https://js202005082300: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@github.com/js202005082300/Aide-memoires.git"
 set d=%date:~0,2%-%date:~3,2%-%date:~6,4%
 set t=%time:~0,2%:%time:~3,2%:%time:~6,2%
 set t=%t: =%
@@ -20,9 +19,9 @@ if not exist ".git" (
 
 if "%~1" NEQ "" ( echo git remote add origin https://js202005082300:%~1@github.com/js202005082300/Aide-memoires.git )
 
-git pull %test%
+git pull
 git add *
 git commit -a -m "%d% %t%"
-git push %test%
+git push
 
 exit
