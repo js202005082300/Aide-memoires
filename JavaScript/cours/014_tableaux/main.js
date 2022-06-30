cfa7b25037d273e9f1e0a2e7c4a2123286d5e778
js <js201910271836@outlook.com>
2022-06-30 10:02:03 +0200

    30-06-22 10:02:01

--- JavaScript/cours/014_tableaux/main.js
+++
@@ -1,9 +0,0 @@
-let someArray = ["Laura", "Bernard", "Franck", "Fatima", "Salim"];
-let anotherArray = [-2, 14, 1, 2, 6, 5, 56, -23, -2, 0];
-
-let result = someArray.map(function(value, index)
-{
-    return value = "Inconnu";
-});
-
-console.log(result);
